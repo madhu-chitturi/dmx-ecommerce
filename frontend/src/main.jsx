1d2b1f1d5b1f2111d1ac7f5f020275a4cb23ed5d
CHITTURI MADHU SEETARAM <106515693+madhu-chitturi@users.noreply.github.com>
2026-01-18 03:12:29 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { RouterProvider } from 'react-router-dom';
-import { router } from './router';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <RouterProvider router={router} />
-);
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
