a12c3315e11ecff5537e6564cad184ed50288281
CHITTURI MADHU SEETARAM <106515693+madhu-chitturi@users.noreply.github.com>
2026-01-18 00:33:54 +0530

    Update orderController.js

--- backend/src/controllers/orderController.js
+++ backend/src/controllers/orderController.js
@@ -1,1 +1,11 @@
+import Order from '../models/Order.js';
 
+export const listUserOrders = async (req, res) => {
+  const orders = await Order.find({ userId: req.user.id });
+  return res.json(orders);
+};
+
+export const listAllOrders = async (req, res) => {
+  const orders = await Order.find();
+  return res.json(orders);
+};
