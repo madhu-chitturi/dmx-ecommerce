00a683a50d92762e5ad4647833d9ea816d8e8fb9
CHITTURI MADHU SEETARAM <106515693+madhu-chitturi@users.noreply.github.com>
2026-01-18 03:32:13 +0530

    Update axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,14 @@
 import axios from 'axios';
+import { useAuthStore } from '../store/authStore';
 
 const instance = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
+});
+
+instance.interceptors.request.use((config) => {
+  const token = localStorage.getItem('dmx_token');
+  if (token) config.headers.Authorization = `Bearer ${token}`;
+  return config;
 });
 
 export default instance;
